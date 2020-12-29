12fbb721e53ae9ab93044639109e3161108d98aa
User <User@ES301N9>
2020-12-29 16:35:12 +0800

    add read data.txt

--- src/com/edu/Test.java
+++ src/com/edu/Test.java
@@ -1,10 +1,19 @@
 package com.edu;
 
+import java.io.*;
+//import java.io.FileRead;
+//import java.io.FileNotFoundException;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-		
+		try {
+			FileReader fis = new FileReader("data.txt");
+			System.out.println("File Open");
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		System.out.print("End");
 	}
 
 }
