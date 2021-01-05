c824cda61f40590c70869f1326552ae5983ff263
User <User@ES301N9>
2021-01-05 16:26:12 +0800

    TEST 2021/01/05

---
+++ src/com/edu/SwitchTest.java
@@ -0,0 +1,21 @@
+package com.edu;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		int x = 1;
+		switch(x) {
+		case 1 :
+			System.out.println("This is 1");
+			break;
+		case 2 :
+			System.out.println("This is 2");
+			break;
+		default:
+			System.out.println("No");
+			break;
+		}
+		System.out.print("Hello World");
+	}
+
+}
